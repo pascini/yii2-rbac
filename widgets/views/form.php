561435b184e99a717c8155ae5a7bcd822d6e5e8f
Pascini <rogerio@pascini.com.br>
2021-11-18 21:01:15 -0300

    Upgrade to Bootstrap 4

--- widgets/views/form.php
+++ widgets/views/form.php
@@ -1,17 +1,17 @@
 <?php
 
-/* 
+/*
  * This file is part of the Dektrium project
- * 
+ *
  * (c) Dektrium project <http://github.com/dektrium>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
 use dektrium\rbac\models\Assignment;
 use kartik\select2\Select2;
-use yii\bootstrap\Alert;
+use yii\bootstrap4\Alert;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
